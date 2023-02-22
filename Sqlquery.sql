6158c9511e8cd313b53f97bc00d2ec336cc54d98
ahmadun <ahmadun.jambi@gmail.com>
2023-02-22 07:36:33 +0700

    db

---
+++ Sqlquery.sql
@@ -0,0 +1,26 @@
+﻿USE [hrddb_sbi]
+GO
+
+/****** Object:  Table [dbo].[Participant_vaccine]    Script Date: 02/22/2023 7:35:06 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Participant_vaccine](
+	[Period] [datetime] NOT NULL,
+	[Nik] [char](6) NOT NULL,
+	[Name] [varchar](90) NOT NULL,
+	[Flag] [char](1) NULL,
+	[EntDate] [datetime] NULL,
+	[UptDate] [datetime] NULL,
+ CONSTRAINT [PK_Participant_vaccine] PRIMARY KEY CLUSTERED 
+(
+	[Period] ASC,
+	[No_vak] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
